d9e750610ad5b887b9b5db610e397e5298376b0c
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-13 11:43:18 -0700

    API call working successfully

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
-// import  from './js/scripts.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import cityTour from './js/city-tour.js';
 
+
+async function makeApiCall(){
+  const response = await cityTour.getTour();
+  console.log(response);
+}
+
+$(document).ready(function (){
+  $("#btn").click(function(event){
+    event.preventDefault(); 
+    makeApiCall();
+  });
+});
