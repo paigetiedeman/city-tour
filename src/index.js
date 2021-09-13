c9c9b8ce63b42ffcf50ab47a4f1a8e9b18d99ec2
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-13 14:47:27 -0700

    Connect the display to the API

--- src/index.js
+++ src/index.js
@@ -6,7 +6,29 @@ import cityTour from "./js/city-tour.js";
 
 async function makeApiCall() {
   const response = await cityTour.getTour();
-  console.log(response);
+  console.log(response.results[0].days[0].itinerary_items);
+  displayResult(response.results[0].days[0].itinerary_items);
+
+}
+
+function displayResult(items) {
+  const pointsHtml = items.map(item => {
+    console.log(item);
+    let imageUrl = 'https://images.unsplash.com/photo-1523665307251-7f4e0e7b23fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80';
+    if(item.poi.images.length > 0) {
+      imageUrl = item.poi.images[0].sizes.thumbnail.url
+    }
+    return `<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="${imageUrl}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${item.poi.name}</h5>
+      <p class="card-text">${item.description}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`;
+  }).join("");
+
+  $("#container").append(pointsHtml);
 }
 
 $(document).ready(function () {
