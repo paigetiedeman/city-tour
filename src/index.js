a7cfd345aff258037a18426c664c36980f825e46
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-14 10:52:14 -0700

    formatting changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import $ from "jquery";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/styles.css";
-import cityTour from "./js/city-tour.js";
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import cityTour from './js/city-tour.js';
 
 async function makeApiCall(city) {
   console.log(city);
   const response = await cityTour.getTour(city);
-  
   displayResult(response.results[0].days[0].itinerary_items);
-
 }
 
 // let city1 = $("#Seattle").val();
@@ -20,12 +18,14 @@ async function makeApiCall(city) {
 // let city6 = $("#Tokyo").val();
 
 function displayResult(items) {
-  const pointsHtml = items.map(item => {
-    let imageUrl = 'https://images.unsplash.com/photo-1523665307251-7f4e0e7b23fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80';
-    if(item.poi.images.length > 0) {
-      imageUrl = item.poi.images[0].sizes.thumbnail.url;
-    }
-    return `<div class="card" style="width: 18rem;">
+  const pointsHtml = items
+    .map((item) => {
+      let imageUrl =
+        'https://images.unsplash.com/photo-1523665307251-7f4e0e7b23fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80';
+      if (item.poi.images.length > 0) {
+        imageUrl = item.poi.images[0].sizes.thumbnail.url;
+      }
+      return `<div class="card">
     <img class="card-img-top" src="${imageUrl}" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">${item.poi.name}</h5>
@@ -33,13 +33,15 @@ function displayResult(items) {
       <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
   </div>`;
-  }).join("");
-  $("#cityDisplay").empty();
-  $("#cityDisplay").append(pointsHtml);
+    })
+    .join('');
+  $('#container').hide();
+  $('#cityDisplay').empty();
+  $('#cityDisplay').append(pointsHtml);
 }
 
 $(document).ready(function () {
-  $(".city").click(function () {
+  $('.city').click(function () {
     const cityName = this.id;
     makeApiCall(cityName);
   });
