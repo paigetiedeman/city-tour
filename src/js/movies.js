e4f24454dc300389fe0a259f488490c9469bc79d
Diego Serafico <dseraficohernandez@gmail.com>
2021-09-15 11:22:01 -0700

    add api calls to get movies and nearest theaters for them

---
+++ src/js/movies.js
@@ -0,0 +1,49 @@
+export class Movies {
+  static getMovies() {
+    const date = new Date().toISOString();
+    return fetch(`https://api-gate2.movieglu.com/filmsNowShowing/?n=3`, {
+      headers: {
+        "api-version": "v200",
+        "Authorization": `${process.env.MOVIES_AUTHORIZATION}`,
+        "x-api-key": `${process.env.MOVIES_API_KEY}`,
+        "device-datetime": `${date}`,
+        "territory": "US",
+        "client": `${process.env.MOVIES_CLIENT}`,
+        "geolocation": "47.6;-122.3"
+      }
+    })
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        throw Error(error);
+      });
+  }
+
+  static getTheaters(filmId) {
+    const date = new Date().toISOString();
+    return fetch(`https://api-gate2.movieglu.com/closestShowing/?film_id=${filmId}&n=3`, {
+      headers: {
+        "api-version": "v200",
+        "Authorization": `${process.env.MOVIES_AUTHORIZATION}`,
+        "x-api-key": `${process.env.MOVIES_API_KEY}`,
+        "device-datetime": `${date}`,
+        "territory": "US",
+        "client": `${process.env.MOVIES_CLIENT}`,
+        "geolocation": "47.6;-122.3"
+      }
+    })
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        throw Error(error);
+      });
+  }
+}
